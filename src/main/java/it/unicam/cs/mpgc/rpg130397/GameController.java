0911d561756bb7be189d2abd814a0e39f39da768
giulio.mandolini <giulio.mandolini@studenti.unicam.it>
2026-04-21 13:09:41 +0200

    Aggiunta della classe JDeserializer per la gestione dei Json rimuovendo la responsabilità dal GameController, creazione del json per memorizzare le informazioni sui diversi nemici

--- src/main/java/it/unicam/cs/mpgc/rpg130397/GameController.java
+++ src/main/java/it/unicam/cs/mpgc/rpg130397/GameController.java
@@ -1,39 +1,27 @@
 package it.unicam.cs.mpgc.rpg130397;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.Reader;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import java.lang.reflect.Type;
 
 public class GameController {
-    private Map<String, Weapon> weapons = new HashMap<>();
+    private Map<String, Weapon> weaponsMap = new HashMap<>();
+    private Map<String, Enemy> enemiesMap = new HashMap<>();
 
     private List<Enemy> enemies;
     private Enemy closestEnemy;
     private long lastUpdate;
     private final float UPDATE_COOLDOWN = 0.05f;
 
-    public GameController() {
+    public GameController() throws FileNotFoundException {
         enemies = new LinkedList<>();
-        getWeapons();
+        weaponsMap = JDeserializer.getWeapons();
+        enemiesMap = JDeserializer.getEnemies();
     }
 
-    //Private method to transform the data contained in the weapons.json file into the weapons hashmap
-    private void getWeapons() throws FileNotFoundException {
-        File f = new File("src/main/resources/Weapons.json");
-        Gson json = new Gson();
-        Reader r = new FileReader(f);
-        //Data type definition for the correct deserialization of the json file
-        Type weaponMapType = new TypeToken<Map<String, Weapon>>() {}.getType();
-        weapons = json.fromJson(r, weaponMapType);
-    }
+
 
     public void updateClosestEnemy()
     {
---
+++ src/main/java/it/unicam/cs/mpgc/rpg130397/JDeserializer.java
@@ -0,0 +1,39 @@
+package it.unicam.cs.mpgc.rpg130397;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.Map;
+
+public class JDeserializer {
+    /**
+     *  Static method that deserializes the data contained in the weapons.json file into the weapons map
+     * @return the map of all the weapons
+     */
+    public static Map<String, Weapon> getWeapons() throws FileNotFoundException {
+        File f = new File("src/main/resources/Weapons.json");
+        Gson json = new Gson();
+        Reader r = new FileReader(f);
+        //Data type definition for the correct deserialization of the json file
+        Type weaponMapType = new TypeToken<Map<String, Weapon>>() {}.getType();
+        return json.fromJson(r, weaponMapType);
+    }
+
+    /**
+     *  Static method that deserializes the data contained in the enemies.json file into the enemies map
+     * @return the map of all the enemies
+     */
+    public static Map<String, Enemy> getEnemies() throws FileNotFoundException {
+        File f = new File("src/main/resources/Enemies.json");
+        Gson json = new Gson();
+        Reader r = new FileReader(f);
+        //Data type definition for the correct deserialization of the json file
+        Type enemyMapType = new TypeToken<Map<String, Enemy>>() {}.getType();
+        return json.fromJson(r, enemyMapType);
+    }
+}
